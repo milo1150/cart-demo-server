9f3abe72c21e2c5c55fc9ed37fbbef8a97684aa4
milo33 <65894437+milo1150@users.noreply.github.com>
2025-02-26 00:19:21 +0700

    update shell cmd

--- dev-stop.sh
+++ dev-stop.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-# sotp traefik
-docker-compose down &&
-	# stop shop-product service
-	cd ../shop-product-service/scripts &&
-	./dev-stop.sh
+WORKSPACE_PATH="$(cd ../ && pwd)"
+echo "WORKSPACE_PATH = $WORKSPACE_PATH"
+
+echo "stop traefik"
+docker-compose down
+
+echo "stop shop-product-service"
+cd "$WORKSPACE_PATH/shop-product-service/scripts" && ./dev-stop.sh
+
+echo "stop cart-service"
+cd "$WORKSPACE_PATH/cart-service/scripts" && ./dev-stop.sh
