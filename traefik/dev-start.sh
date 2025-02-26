899594d60670b78708f77f9b39e2bfdad448b84a
milo33 <65894437+milo1150@users.noreply.github.com>
2025-02-26 17:43:38 +0700

    move to trafik folder

---
+++ traefik/dev-start.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+WORKSPACE_PATH="$(cd ../../ && pwd)"
+echo "WORKSPACE_PATH = $WORKSPACE_PATH"
+
+echo "start traefik"
+docker-compose up -d
+
+echo "start shop-product-service"
+cd "$WORKSPACE_PATH/shop-product-service/scripts" && ./dev-start-detach.sh
+
+echo "start cart-service"
+cd "$WORKSPACE_PATH/cart-service/scripts" && ./dev-start-detach.sh
---
+++ traefik/traefik-stop.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+echo "stop traefik"
+docker-compose down
