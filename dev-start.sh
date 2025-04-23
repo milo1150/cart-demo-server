dfa6b5dd7e6906b11f0a1e032b911d867c6e24c1
milo33 <65894437+milo1150@users.noreply.github.com>
2025-04-23 07:44:07 +0700

    reorder run cmds

--- dev-start.sh
+++ dev-start.sh
@@ -6,14 +6,14 @@ echo "WORKSPACE_PATH = $WORKSPACE_PATH"
 echo "start caddy"
 docker-compose up -d
 
-echo "start shop-product-service"
-cd "$WORKSPACE_PATH/shop-product-service/scripts" && ./dev-start-detach.sh
-
-echo "start cart-service"
-cd "$WORKSPACE_PATH/cart-service/scripts" && ./dev-start-detach.sh
-
 echo "start user-service"
 cd "$WORKSPACE_PATH/user-service/scripts" && ./dev-start-detach.sh
 
+echo "start shop-product-service"
+cd "$WORKSPACE_PATH/shop-product-service/scripts" && ./dev-start-detach.sh
+
 echo "start payment-service"
 cd "$WORKSPACE_PATH/payment-service/scripts" && ./dev-start-detach.sh
+
+echo "start cart-service"
+cd "$WORKSPACE_PATH/cart-service/scripts" && ./dev-start-detach.sh
--- dev-stop.sh
+++ dev-stop.sh
@@ -6,14 +6,14 @@ echo "WORKSPACE_PATH = $WORKSPACE_PATH"
 echo "stop caddy"
 docker-compose down
 
-echo "stop shop-product-service"
-cd "$WORKSPACE_PATH/shop-product-service/scripts" && ./dev-stop.sh
-
-echo "stop cart-service"
-cd "$WORKSPACE_PATH/cart-service/scripts" && ./dev-stop.sh
-
 echo "stop user-service"
 cd "$WORKSPACE_PATH/user-service/scripts" && ./dev-stop.sh
 
+echo "stop shop-product-service"
+cd "$WORKSPACE_PATH/shop-product-service/scripts" && ./dev-stop.sh
+
 echo "start payment-service"
 cd "$WORKSPACE_PATH/payment-service/scripts" && ./dev-stop.sh
+
+echo "stop cart-service"
+cd "$WORKSPACE_PATH/cart-service/scripts" && ./dev-stop.sh
